78a91ee3243ad97a4d7478827627c66d723c0e44
Ertugrul Karademir <ekarademir@gmail.com>
2017-08-23 17:59:08 +0100

    SDL Initialisation

--- TetrSDL/main.c
+++ TetrSDL/main.c
@@ -3,13 +3,23 @@
 //  TetrSDL
 //
 //  Created by Ertugrul Karademir on 23/08/2017.
-//  Copyright © 2017 RotatingWave. All rights reserved.
+//  Copyright © 2017 Ertugrul Karademir. See LICENCE for licence info.
 //
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
+    // Initialise subsystems
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL Init Error: %s", SDL_GetError());
+    }
+    
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Initialised SDL!\n");
+    
+    SDL_Quit();
     return 0;
 }
