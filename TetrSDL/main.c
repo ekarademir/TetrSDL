5957f8c64d6da445cae4dcb9100799d8dc50ff40
Ertugrul Karademir <ekarademir@gmail.com>
2017-08-23 17:51:25 +0100

    Initial Commit

---
+++ TetrSDL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TetrSDL
+//
+//  Created by Ertugrul Karademir on 23/08/2017.
+//  Copyright © 2017 RotatingWave. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
